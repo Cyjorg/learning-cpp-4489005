026f2c5642e4181d0141b4bb8f19743b742f312c
Cyjorg <cindyjorgensen07@gmail.com>
2024-06-05 21:47:48 +0000

    Challenge Solution - Chapter 01

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -1,14 +1,15 @@
 // Learning C++ 
-// Challenge 01_03
+// Challenge 01_03 with changes
 // Console Interaction, by Eduardo Corpeño 
 
 #include <iostream>
 #include <string>
 
 int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str;
+    std::string name;
+    std::cout << "Type your name: ";
+    std::cin >> name;
+    std::cout << "Hello " << name;
 
     std::cout << std::endl << std::endl;
     return (0);
